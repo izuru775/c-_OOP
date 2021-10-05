ccc1d453ef68c15b976cf4a23c2d6177d3fd0a19
izuru775 <izuru775@gmail.com>
2021-10-06 10:47:01 +1100

    Initializing fields with constructors

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -11,6 +11,23 @@ namespace Packt.Shared
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
         public const string Species = "Homo Sapien";
+        // Read only fields
         public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        // Constructor
+        public Person()
+        {
+            // Set default values for fields, including read-only fields
+            Name = "Unkown";
+            Instantiated = DateTime.Now;
+        }
+        public Person(string initialName, string homePlanet)
+        {
+            // Set default values for fields, including read-only fields
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
     }
 }
--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -47,31 +47,31 @@ namespace PeopleApp
             bob.Children.Add(new Person { Name = "Alfred" });
             bob.Children.Add(new Person { Name = "Zoe" });
             WriteLine($"{bob.Name} has {bob.Children.Count} children:");
-           
+
             foreach (var child in bob.Children)
             {
                 WriteLine($"{child.Name}");
             }
 
             WriteLine();
-            BankAccount.InterestRate =0.012M; // Store a shared value
+            BankAccount.InterestRate = 0.012M; // Store a shared value
 
             var jonesAccount = new BankAccount();
             jonesAccount.AccountName = "Mrs.Jones";
             jonesAccount.Balance = 2400;
             WriteLine(
-                format:"{0} earned {1:C} interest.",
-                arg0:jonesAccount.AccountName,
-                arg1:jonesAccount.Balance*BankAccount.InterestRate
+                format: "{0} earned {1:C} interest.",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate
             );
 
-            var gerrierAccount =new BankAccount();
+            var gerrierAccount = new BankAccount();
             gerrierAccount.AccountName = "Ms.Gerrier";
             gerrierAccount.Balance = 98;
             WriteLine(
-                format:"{0} earned {1:C} interest.",
+                format: "{0} earned {1:C} interest.",
                 arg0: gerrierAccount.AccountName,
-                arg1:gerrierAccount.Balance*BankAccount.InterestRate
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate
             );
 
             WriteLine();
@@ -80,6 +80,22 @@ namespace PeopleApp
             WriteLine();
             WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
 
+            WriteLine();
+            var blankPerson = new Person();
+            WriteLine(
+                format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
+
+            WriteLine();
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(
+                format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated
+            );
         }
     }
 }
